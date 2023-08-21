fb71a13c969218280a08da1ed9e187534498c571
路西法 <zhaiyuxin103@hotmail.com>
2023-08-21 12:55:48 +0800

    build(deps): add opcodesio log viewer

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,11 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Opcodes\LogViewer\LogFile;
+use Opcodes\LogViewer\LogFolder;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,34 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $users = [
+            'zhaiyuxin103@gmail.com',
+            'zhaiyuxin103@hotmail.com',
+        ];
+
+        Gate::define('viewLogViewer', function (?User $user) use ($users) {
+            // return true if the user is allowed access to the Log Viewer
+            return in_array(optional($user)?->email, $users);
+        });
+
+        Gate::define('downloadLogFile', function (?User $user, LogFile $file) use ($users) {
+            // return true if the user is allowed to download the specific log file.
+            return in_array(optional($user)?->email, $users);
+        });
+
+        Gate::define('downloadLogFolder', function (?User $user, LogFolder $folder) use ($users) {
+            // return true if the user is allowed to download the whole folder.
+            return in_array(optional($user)?->email, $users);
+        });
+
+        Gate::define('deleteLogFile', function (?User $user, LogFile $file) use ($users) {
+            // return true if the user is allowed to delete the specific log file.
+            return in_array(optional($user)?->email, $users);
+        });
+
+        Gate::define('deleteLogFolder', function (?User $user, LogFolder $folder) use ($users) {
+            // return true if the user is allowed to delete the whole folder.
+            return in_array(optional($user)?->email, $users);
+        });
     }
 }

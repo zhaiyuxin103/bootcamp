518b8a8f3b75f1c7f797697ea193ab35bbdec1c4
路西法 <zhaiyuxin103@hotmail.com>
2023-08-21 09:46:29 +0800

    build(deps): add laravel telescope

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
 
         $schedule->command('horizon:snapshot')->everyFiveMinutes();
+        $schedule->command('telescope:prune')->daily();
     }
 
     /**
